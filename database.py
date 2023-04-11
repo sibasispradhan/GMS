bd1fd3b8920a2c42af733b14b8318a38d62e4874
sibasispradhan <125789985+sibasispradhan@users.noreply.github.com>
2023-04-11 19:16:13 +0000

    db connection establish

--- database.py
+++ database.py
@@ -7,3 +7,15 @@ engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
                          "ssl_ca": "/etc/ssl/cert.pem"
                        }})
+def login_from_db(username):
+  with engine.connect() as conn:
+    query = "SELECT * FROM info WHERE username = '" + username + "'"
+    result = conn.execute(text(query))
+    rows = result.all()
+    info = []
+    for row in rows:
+      info.append(row._mapping)
+    if len(rows) == 0:
+      return None
+    else:
+      return info
